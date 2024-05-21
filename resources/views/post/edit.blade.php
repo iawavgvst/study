85a8cc371f84afd5ad1c7c5e73cb8a2b22e1c67d
iawavgvst <alkelin1@gmail.com>
2024-05-22 02:58:11 +0300

    crud modification interface

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -6,15 +6,40 @@
             @method('patch')
             <div class="form-group">
                 <label for="title">title</label>
-                <input type="text" name="title" class="form-control" id="title" placeholder="something interesting" value="{{ $post->title }}">
+                <input type="text" name="title" class="form-control" id="title" placeholder="something interesting"
+                       value="{{ $post->title }}">
             </div>
             <div class="form-group">
                 <label for="content">content</label>
-                <textarea name="content" class="form-control" id="content" placeholder="some content">{{ $post->content }}</textarea>
+                <textarea name="content" class="form-control" id="content"
+                          placeholder="some content">{{ $post->content }}</textarea>
             </div>
             <div class="form-group">
                 <label for="image">image</label>
-                <input name="image" type="text" class="form-control" id="image" value="{{ $post->image }}" placeholder="some image">
+                <input name="image" type="text" class="form-control" id="image" value="{{ $post->image }}"
+                       placeholder="some image">
+            </div>
+            <div class="form-group">
+                <label for="category">category</label>
+                <select class="form-control" id="category" name="category_id">
+                    @foreach($categories as $category)
+                        <option
+                                {{ $category->id === $post->category->id ? ' selected' : '' }}
+                                value="{{ $category->id }}">{{ $category->title }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="tags">tags</label>
+                <select multiple class="form-control" id="tags" name="tags[]">
+                    @foreach($tags as $tag)
+                        <option
+                                @foreach($post->tags as $postTag)
+                                    {{ $tag->id === $postTag->id ? ' selected' : '' }}
+                                @endforeach
+                                value="{{ $tag->id }}">{{ $tag->title }}</option>
+                    @endforeach
+                </select>
             </div>
             <button type="submit" class="btn btn-primary mt-3">update</button>
         </form>
