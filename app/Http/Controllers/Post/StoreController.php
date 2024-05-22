4eadd64c49e301ffda40b5cf60748a843415a7aa
iawavgvst <alkelin1@gmail.com>
2024-05-22 18:38:27 +0300

    class service

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -5,17 +5,15 @@ namespace App\Http\Controllers\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
+use Faker\Provider\Base;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request) // реквест нужно вставить в виде аргумента
     {
         $data = $request->validated();
-        $tags = $data['tags'];
-        unset($data['tags']);
 
-        $post = Post::create($data);
-        $post->tags()->attach($tags);
+        $this->service->store($data);
 
         return redirect()->route('post.index');
     }
